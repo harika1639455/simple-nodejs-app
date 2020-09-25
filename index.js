b5f8d562f71badd2a4ac77d48b87fca068e04f29
Manikanth Reddy Abbu <53967637+ManikanthReddyAbbu@users.noreply.github.com>
2020-09-25 20:14:55 +0530

    msg - Hi there !!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send(' Welcome to demo !! \n');
+  res.send(' Hi there!! I am using CloudNative tools \n');
 });
 
 var server = app.listen(3000, function () {
